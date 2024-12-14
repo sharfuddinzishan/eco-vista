7993f9f5277d542ef1c1b9ed0fff9b9dfa2509c5
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2024-12-14 22:32:35 +0600

    done

--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -10,19 +10,19 @@ export default function LayoutLocation({
 }) {
   return (
     <>
-      <div class="wrapper">
-        <div class="overlay h-full"></div>
+      <div className="wrapper">
+        <div className="overlay h-full"></div>
         <Image
           src="/assets/images/background.png"
-          class="bg-img h-full"
+          className="bg-img h-full"
           width={700}
           height={1200}
           alt="nature-background"
         />
 
-        <main class="!z-50 w-full">
-          <div class="container">
-            <div class="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+        <main className="!z-50 w-full">
+          <div className="container">
+            <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
               {children}
               {temperature}
               {wind}
